c0e3409a1c4d0b142dbfd6d999c9d3570a36e166
em <emilie.grandjean@epfl.ch>
2024-04-03 13:14:13 +0200

    for intermediate presentation

--- python_thymio/Intermediate_presentation/main.py
+++ python_thymio/Intermediate_presentation/main.py
@@ -15,35 +15,14 @@ from classes import Thymio
 # import W6_T1_PS_24_04_02
 # import W6_T1_PS_24_04_03
 
-import 24_03_30_doubledCostume
-import 24_04_03_allPrograms
-import 24_04_03_modifiedCostume
+import doubledCostume_24_03_30
+import allPrograms_24_04_03
+import modifiedCostume_24_04_03
 
 client = ClientAsync()
 node = aw(client.wait_for_node())
-# aw(node.unlock())
 aw(node.lock())
 aw(node.wait_for_variables())
-
-# aw(node.register_events([("StopnUnlock", )]))
-
-# program = """
-# onevent StopnUnlock
-#     24_03_30_doubledCostume.setButtons(robot, 0)
-
-#     print(robot.button_center)
-#     24_04_03_allPrograms.stop_program(robot, node, motor_speed=0)
-#     aw(node.unlock())
-# """
-
-# # The event data are obtained from variable event.args:
-# program = """
-# onevent StopnUnlock
-#     motor.left.target = event.args[0]
-#     motor.right.target = event.args[1]
-# """
-# aw(node.compile(program))
-# aw(node.run())
 
 
 #Classes initialization
@@ -51,124 +30,68 @@ robot = Thymio()
 
 def update_sensors_data(robot, node):
 
-    # get button values
+    # get proximity & button values
     robot.getProxHorizontal(node)
     robot.getButtons(node)
 
-# def thread_buttonCenter(name) :
-
-#     logging.info("Thread %s: starting", name)
-#     time.sleep(2)
-
-#     # aw(node.send_events({"StopnUnlock": }))
-
-#     logging.info("Thread %s: finishing", name)
-
-# def thread_buttonCenter(event) :
-
-#     if (robot.button_center) :
-#         # robot.buttonForward = 0
-#         W6_T1_PS_24_03_30.setButtons(robot, 0)
-
-#         print(robot.button_center)
-#         W6_T1_PS_24_03_30.stop_program(robot, node, motor_speed=0)
-#         aw(node.unlock())
-        # break
-
-
-# def main() :
-
-    # INTHREAD = threading.Event()
-
-    # threadToStop = threading.Thread(target=thread_buttonCenter, args=(INTHREAD,))
-
-    # threadToStop.start()
 
 while(1) :
 
-    # aw(client.sleep(0.1))
     update_sensors_data(robot, node)
-
     robot.setLEDTop(node, [32,32,32])
-
-    # format = "%(asctime)s: %(message)s"
-    # logging.basicConfig(format=format, level=logging.INFO,
-    #                     datefmt="%H:%M:%S")
-
-    # logging.info("Main    : before creating thread")
-    # x = threading.Thread(target=thread_buttonCenter, args=(1,))
-    # logging.info("Main    : before running thread")
-    # x.start()
-    # logging.info("Main    : wait for the thread to finish")
-    # # x.join()
-    # logging.info("Main    : all done")
-
-    # W6_T1_PS_24_04_02.acc()
 
 ########
 
     if (robot.button_center) :
 
-        # robot.buttonForward = 0
-        24_03_30_doubledCostume.setButtons(robot, 0)
+        doubledCostume_24_03_30.setButtons(robot, 0)
 
         print(robot.button_center)
-        24_04_03_allPrograms.stop_program(robot, node, motor_speed=0)
+        allPrograms_24_04_03.stop_program(robot, node, motor_speed=0)
         aw(node.unlock())
         break
 
 ######## MODIFIED COSTUME
 
-    prox = list(node["prox.horizontal"]) + [0]
+    # prox = list(node["prox.horizontal"]) + [0]
 
-    if(prox[2]) :
-        update_sensors_data(robot, node)
-        print(prox[2])
+    # if(prox[2]) :
+    #     update_sensors_data(robot, node)
+    #     print(prox[2])
 
-        24_04_03_modifiedCostume.see_costume(robot, node, motor_speed=50)
-    else :
-        24_04_03_modifiedCostume.no_costume(robot, node, motor_speed=0)
-        print(prox[2])
+    #     modifiedCostume_24_04_03.see_costume(robot, node, motor_speed=50)
+    # else :
+    #     modifiedCostume_24_04_03.no_costume(robot, node, motor_speed=0)
+    #     print(prox[2])
 
 ######## DOUBLED COSTUME
 
-    # 24_03_30_doubledCostume.ext_interaction(robot, node, motor_speed=100, obs_threshold=500)
+    doubledCostume_24_03_30.ext_interaction(robot, node, motor_speed=100, obs_threshold=500)
 
-    # if (robot.button_forward and not(robot.buttonForward)) :
+    if (robot.button_forward and not(robot.buttonForward)) :
 
-    #     24_03_30_doubledCostume.setButtons(robot, 0)
+        doubledCostume_24_03_30.setButtons(robot, 0)
 
-    #     robot.buttonForward = 1
+        robot.buttonForward = 1
 
+    elif (robot.button_forward and robot.buttonForward) :
 
-    # elif (robot.button_forward and robot.buttonForward) :
+        robot.buttonForward = 0
+        robot.setLEDTop(node, [0,0,32])
+        aw(client.sleep(2))
 
-    #     robot.buttonForward = 0
-    #     robot.setLEDTop(node, [0,0,32])
-    #     aw(client.sleep(2))
+    if (robot.buttonForward) :
 
-
-    # if (robot.buttonForward) :
-
-    #     # while (not robot.button_center) :
-
-    #     24_03_30_doubledCostume.programFront(robot, node, client)
+        doubledCostume_24_03_30.programFront(robot, node, client)
 
 ########
 
-    # if (robot.button_backward) :
+    if (robot.button_backward) :
 
-    #     24_03_30_doubledCostume.setButtons(robot, 0)
+        doubledCostume_24_03_30.setButtons(robot, 0)
 
-    #     robot.buttonBackward = 1
+        robot.buttonBackward = 1
 
-    # if (robot.buttonBackward) :
+    if (robot.buttonBackward) :
 
-    #     24_03_30_doubledCostume.programBack(robot, node, client)
-
-# INTHREAD.set()
-# threadToStop.join()
-
-
-# if __name__ == "__main__":
-#         main()
+        doubledCostume_24_03_30.programBack(robot, node, client)
